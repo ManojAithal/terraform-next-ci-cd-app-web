9a70d223cbf220a585a7dbdcc37c87d37cd9aba8
ManojAithal <manojaithal026@gmail.com>
2025-03-01 16:21:19 +0530

    nextJs Config updated 2.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/terraform-next-ci-cd-app-web', // Replace with your actual repo name
     images: {
       unoptimized: true, // Required if using Next.js images on GitHub Pages
     },
